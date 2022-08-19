335939da58c7e4fc4aee8b7988e9d5f6cd6f1b00
mdrhmn <mdrhmn99@gmail.com>
2022-08-19 22:24:02 +0800

    Updated About Me section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,8 +6,8 @@ import Intro from '../Intro'
 
 export const Wrapper = styled.section`
   display: grid;
-  grid-template-columns: repeat(8, 1fr);
-  grid-template-rows: repeat(5, 1fr);
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(1, 1fr);
   position: relative;
   ${props => props.theme.media.max.sm`
     grid-template-columns: 100%;
@@ -17,8 +17,8 @@ export const Wrapper = styled.section`
 `
 
 export const Information = styled.header`
-  grid-row: 2 / 4;
-  grid-column: 1 / 6;
+  grid-row: 2 / 3;
+  grid-column: 1 / 4;
   z-index: 3;
   ${props => props.theme.media.max.md`
     grid-column: 1 / 7;
